7384586fcc85807725d37a18a288004874af231c
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-05-20 00:12:37 +0200

    Add class for position system

---
+++ RPG2DGameDungeonCrawler/Assets/Scripts/Utils/Position.cs
@@ -0,0 +1,15 @@
+public struct Position
+{
+    public int x;
+    public int y;
+    public Position (int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + " x:" + x + " y:" + y;
+    }
+}
