258e6c8e30186241da08725f2f6d46befb0897f7
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-05-20 00:12:51 +0200

    Add new dungeon class

---
+++ RPG2DGameDungeonCrawler/Assets/Scripts/Components/DungeonComponent.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonComponent
+{
+    public uint RoomsNumber { get; private set; }
+    public List<RoomComponent> Rooms { get; private set; } = new List<RoomComponent>();
+    private LevelController _levelController;
+    public DungeonController Controller { get; private set; }
+
+    public DungeonComponent(LevelController levelController)
+    {
+        _levelController = levelController;
+        Controller = new DungeonController(this);
+    }
+
+    public uint CalcRoomsNumber()
+    {    
+        uint levelConstant = 5;
+        uint levelNumber = _levelController.LevelNumber;
+        uint rand = (uint) Math.Floor((double) UnityEngine.Random.Range(0, 3));
+        double levelMultiplier = 1.25;
+        
+        return (uint) Math.Floor((double) levelNumber * levelMultiplier) + levelConstant + rand;
+    }
+
+    public void Generate()
+    {
+        RoomsNumber = CalcRoomsNumber();
+        Controller.GenerateRooms();
+    }
+}
