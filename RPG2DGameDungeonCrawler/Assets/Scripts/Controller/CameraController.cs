eecc92dd69585ca4b7148f0a909d10c70caba520
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-05-22 00:15:02 +0200

    Reworked camera controller, fixed bugs

--- RPG2DGameDungeonCrawler/Assets/Scripts/Controller/CameraController.cs
+++ RPG2DGameDungeonCrawler/Assets/Scripts/Controller/CameraController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,71 +6,76 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
-    public float smoothSpeed;
+    public Character PlayerController;
     private Vector3 newPos, initPos, targetPos;
     float newX, newY, newPlayerX, newPlayerY, elapsed = 0, duration = 1f;
-    bool active;
+    private Vector3 _targetCameraPos, _initPos;
+    private float _cameraNewX, _cameraNewY, _playerNewX, _playerNewY, _elapsed, _duration;
+    private bool _active;
 
     void Start()
     {
-        newPos = initPos;
+        _active = false;
+        _elapsed = 0;
+        _duration = 1f;
     }
 
     void Update()
     {   
-        if(active)
+        if(_active)
         {
-            elapsed += Time.deltaTime;
-            float completion = elapsed / duration;
-            transform.position = Vector3.Lerp(initPos, targetPos, completion);
-            if(transform.position == targetPos){
-                active = false;
-                elapsed = 0;
+            _elapsed += Time.deltaTime;
+            float completion = _elapsed / _duration;
+            transform.position = Vector3.Lerp(_initPos, _targetCameraPos, completion);
+            if(transform.position == _targetCameraPos){
+                _active = false;
+                _elapsed = 0;
                 player.transform.position = new Vector3(
-                    newPlayerX,
-                    newPlayerY,
+                    _playerNewX,
+                    _playerNewY,
                     player.transform.position.z
                 );
+                PlayerController.LockControlls = false;
             } 
         }
     }
 
-    public void Translate(Directions direction)
+    public void Translate(Directions direction, Vector3 init)
     {
-        active = true;
-        initPos = transform.position;
-        
+        _active = true;
+        _initPos = init;
+
         switch(direction){
             case Directions.Up:
-                newX = initPos.x;
-                newY = initPos.y + (float) Room.baseRoomHeight;
-                newPlayerX = player.transform.position.x;
-                newPlayerY = player.transform.position.y + 3;
+                _cameraNewX = init.x;
+                _cameraNewY = init.y + (float) RoomComponent.Height;
+                _playerNewX = player.transform.position.x;
+                _playerNewY = player.transform.position.y + 3;
             break;
             case Directions.Down:
-                newX = initPos.x;
-                newY = initPos.y - (float) Room.baseRoomHeight;
-                newPlayerX = player.transform.position.x;
-                newPlayerY = player.transform.position.y - 3;
+                _cameraNewX = init.x;
+                _cameraNewY = init.y - (float) RoomComponent.Height;
+                _playerNewX = player.transform.position.x;
+                _playerNewY = player.transform.position.y - 3;
             break;
             case Directions.Right:
-                newX = initPos.x + (float) Room.baseRoomWidth;
-                newY = initPos.y;
-                newPlayerX = player.transform.position.x + 2;
-                newPlayerY = player.transform.position.y;
+                _cameraNewX = init.x + (float) RoomComponent.Width;
+                _cameraNewY = init.y;
+                _playerNewX = player.transform.position.x + 2;
+                _playerNewY = player.transform.position.y;
             break;
             case Directions.Left:
-                newX = initPos.x - (float) Room.baseRoomWidth; 
-                newY = initPos.y;
-                newPlayerX = player.transform.position.x - 2;
-                newPlayerY = player.transform.position.y;
+                _cameraNewX = init.x - (float) RoomComponent.Width;
+                _cameraNewY = init.y;
+                _playerNewX = player.transform.position.x - 2;
+                _playerNewY = player.transform.position.y;
             break;
         }
 
-        targetPos = new Vector3(
-            newX,
-            newY,
-            initPos.z
+        _targetCameraPos = new Vector3(
+            _cameraNewX,
+            _cameraNewY,
+            init.z
         );
     }
 }
