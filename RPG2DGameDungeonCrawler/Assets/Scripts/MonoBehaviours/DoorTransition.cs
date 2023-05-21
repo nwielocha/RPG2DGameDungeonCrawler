0cf8a0cb2048f08eaffa71b80fadc04fe120e535
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-05-22 00:12:32 +0200

    Restyled door transition script

--- RPG2DGameDungeonCrawler/Assets/Scripts/MonoBehaviours/DoorTransition.cs
+++ RPG2DGameDungeonCrawler/Assets/Scripts/MonoBehaviours/DoorTransition.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
   
-  public class DoorTransition: MonoBehaviour
+  public class DoorTransition : MonoBehaviour
     {
 
-        GameObject _mainCamera;
-        CameraController _cameraController;
+        private GameObject _mainCamera;
+        private CameraController _cameraController;
         public void Action(Directions direction)
         {
           _mainCamera = GameObject.FindWithTag("MainCamera");
           if(_mainCamera != null){
             _cameraController = _mainCamera.GetComponent<CameraController>();
-            _cameraController.Translate(direction);
+            _cameraController.Translate(direction, _mainCamera.transform.position);
           }
         }
     }
