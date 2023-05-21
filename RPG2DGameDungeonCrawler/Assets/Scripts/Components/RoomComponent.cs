2870a9c4861ab1d6ead6bb394a95bd56dc492a2c
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-05-22 00:16:46 +0200

    Extended room component

--- RPG2DGameDungeonCrawler/Assets/Scripts/Components/RoomComponent.cs
+++ RPG2DGameDungeonCrawler/Assets/Scripts/Components/RoomComponent.cs
@@ -1,18 +1,21 @@
 public class RoomComponent
 {
+    public DungeonComponent Dungeon { get; private set; }
     public Position Pos { get; private set; }
     public RoomType Type { get; set; }
     public const uint Width = 15;
-    public const uint Height = 15;
+    public const uint Height = 9;
  
-    public RoomComponent(Position pos)
+    public RoomComponent(Position pos, DungeonComponent dun)
     {
         Pos = pos;
+        Dungeon = dun;
     }
 
-    public RoomComponent(Position pos, RoomType type)
+    public RoomComponent(Position pos, DungeonComponent dun, RoomType type)
     {
         Pos = pos;
         Type = type;
+        Dungeon = dun;
     }
 }
