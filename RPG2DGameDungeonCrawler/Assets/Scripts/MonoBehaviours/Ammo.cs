c211fa2bf93108b1b1cbc04d7fa6e50b2b45a89c
Nicolas Wielocha <46528520+nwielocha@users.noreply.github.com>
2023-05-29 16:31:43 +0200

    initial update

--- RPG2DGameDungeonCrawler/Assets/Scripts/MonoBehaviours/Ammo.cs
+++ RPG2DGameDungeonCrawler/Assets/Scripts/MonoBehaviours/Ammo.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 public class Ammo : MonoBehaviour
 {
 	public int damageInflicted;
-	Rigidbody2D rb2D;
-
-	private void Awake()
-	{
-		rb2D = GetComponent<Rigidbody2D>();
-	}
 
 	void OnTriggerEnter2D(Collider2D collision)
 	{
@@ -22,7 +16,6 @@ public class Ammo : MonoBehaviour
 		}
 		else if (collision.gameObject.CompareTag("Obstacle"))
 		{
-			print("kutrwaasssssss");
 			gameObject.SetActive(false);
 		}
 	}
