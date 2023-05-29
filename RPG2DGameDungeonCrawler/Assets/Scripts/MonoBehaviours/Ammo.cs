576371d38b055c50ebd823891753866d6a33c59f
Nicolas Wielocha <46528520+nwielocha@users.noreply.github.com>
2023-05-29 16:13:01 +0200

    Update Ammo / Obstacle collision
    Somehow its working like it suppose to xD

--- RPG2DGameDungeonCrawler/Assets/Scripts/MonoBehaviours/Ammo.cs
+++ RPG2DGameDungeonCrawler/Assets/Scripts/MonoBehaviours/Ammo.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class Ammo : MonoBehaviour
 {
 	public int damageInflicted;
+	Rigidbody2D rb2D;
+
+	private void Awake()
+	{
+		rb2D = GetComponent<Rigidbody2D>();
+	}
 
 	void OnTriggerEnter2D(Collider2D collision)
 	{
@@ -12,6 +18,11 @@ public class Ammo : MonoBehaviour
 
 			StartCoroutine(enemy.DamageCharacter(damageInflicted, 0.0f));
 
+			gameObject.SetActive(false);
+		}
+		else if (collision.gameObject.CompareTag("Obstacle"))
+		{
+			print("kutrwaasssssss");
 			gameObject.SetActive(false);
 		}
 	}
