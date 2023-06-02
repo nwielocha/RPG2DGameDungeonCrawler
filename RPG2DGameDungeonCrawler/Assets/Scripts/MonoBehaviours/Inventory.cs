7a34d0ab764f771663ca7ecba27a47f1b97e19b8
Szymon Poszwa <115754674+sposzwa@users.noreply.github.com>
2023-06-02 18:32:43 +0200

    Added helper functions to the inventory

--- RPG2DGameDungeonCrawler/Assets/Scripts/MonoBehaviours/Inventory.cs
+++ RPG2DGameDungeonCrawler/Assets/Scripts/MonoBehaviours/Inventory.cs
@@ -14,6 +14,35 @@ public class Inventory : MonoBehaviour
 		CreateSlots();
 	}
 
+	void Update()
+	{
+		UpdateUI();
+	}
+
+	public void UpdateUI()
+	{
+		for (int i = 0; i < items.Length; i++)
+		{
+			if (items[i] != null)
+			{
+				Debug.Log(items[i].quantity);
+				Slot slotScript = slots[i].GetComponent<Slot>();
+				Text quantityText = slotScript.qtyText;
+				if(items[i].quantity > 0)
+				{
+					itemImages[i].enabled = true;
+					quantityText.enabled = true;
+					quantityText.text = items[i].quantity.ToString();
+				}
+				else
+				{
+					quantityText.enabled = false;
+					itemImages[i].enabled = false;
+				}
+			}
+		}
+	}
+
 	public void CreateSlots()
 	{
 		if (slotPrefab != null)
@@ -25,6 +54,38 @@ public class Inventory : MonoBehaviour
 				newSlot.transform.SetParent(gameObject.transform.GetChild(0).transform); // obiekt podrzedny: InventoryBackground
 				slots[i] = newSlot;
 				itemImages[i] = newSlot.transform.GetChild(1).GetComponent<Image>(); // obiekt podrzedny: ItemImage
+			}
+		}
+	}
+
+	public int PlayerGold()
+	{
+		foreach(Item item in items)
+		{
+			if(item!=null)
+			{
+				if(item.itemType == ItemType.COIN)
+				{
+					return item.quantity;
+				}
+			}
+		}
+		return 0;
+	}
+
+	public void RemoveGold(int amount)
+	{
+		foreach(Item item in items)
+		{
+			if(item!=null)
+			{
+				if(item.itemType == ItemType.COIN)
+				{
+					Debug.Log(amount);
+					Debug.Log(item.quantity);
+
+					item.quantity -= amount;
+				}
 			}
 		}
 	}
